59d42e4c7361cd1008a17986f2ec34e1d22bd668
Brian Emil Hartz <brianhartz@gmail.com>
2014-05-13 14:04:17 -0600

    added css and working on creating dom return element

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-
-var languages = [
-	{name: 'English',
-	code: 'eng'},
-	{name: 'Spanish',
-	code: 'spa'},
-	{name: 'German',
-	code: 'ger'},
-	{name: 'French',
-	code: 'fra'}
-]
+var languages = [{
+    name: 'English',
+    code: 'eng'
+}, {
+    name: 'Spanish',
+    code: 'spa'
+}, {
+    name: 'German',
+    code: 'ger'
+}, {
+    name: 'French',
+    code: 'fra'
+}]
 
 
 var express = require('express');
@@ -18,7 +20,7 @@ var BeGlobal = require('node-beglobal');
 
 //initialize the BeGlobal API
 var beglobal = new BeGlobal.BeglobalAPI({
-  api_token: 'FGtvXoNFMD0AV075EHTVSw%3D%3D'
+    api_token: 'FGtvXoNFMD0AV075EHTVSw%3D%3D'
 });
 
 var app = express();
@@ -28,60 +30,42 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
 app.get('/', function(req, res) {
-	res.render('index');
+    res.render('index');
 });
 
 app.get('/translate', function(req, res) {
-	res.render('Translate', {langs: languages});
+    res.render('Translate', {
+        langs: languages
+    });
 })
 
 app.get('/quiz', function(req, res) {
-	res.render('Quiz');
+    res.render('Quiz');
 })
 
 app.get('/progress', function(req, res) {
-	res.render('Progress');
+    res.render('Progress');
 })
 
 app.post('/translation', function(req, res) {
-	console.log(req);
-	beglobal.translations.translate(
-  {text: req.body.info.text, from: req.body.info.from, to: req.body.info.to},
-  function(err, results) {
-    if (err) {
-      return console.log(err);
-    }
+    console.log(req);
+    beglobal.translations.translate({
+            text: req.body.info.text,
+            from: req.body.info.from,
+            to: req.body.info.to
+        },
+        function(err, results) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(results);
+            res.send(results.translation);
 
-    console.log(results);
-		res.send(results.translation);
-
-  }
-);
-	// res.send(req.body);
+        }
+    );
+    // res.send(req.body);
 })
 
 var server = app.listen(3364, function() {
-	console.log('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + server.address().port);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- public/js/translate.js
+++ public/js/translate.js
@@ -1,29 +1,44 @@
 $(document).ready(function() {
 
-	$(document).on('click', '#translate-btn', function() {
-		var text = $('#translate-input').val();
-		var from = $('#start-lang option:selected').data('code');
-		var to = $('#end-lang option:selected').data('code');
-		console.log(text, from, to);
-		$.post('/translation', {info: {text: text, from: from, to: to}}, function(data) {
-			console.log(data);
-		})
+    var createTranslateEl = function(inputLang, outputLang, translationInput, translationOutput) {
+        var translationEl = $('.template.translation').clone();
 
-		return false;
-	})
+        translationEl.find('.input-lang').text(inputLang);
+        translationEl.find('.output-lang').text(outputLang);
+
+        translationEl.find('.translate-input').text(translationInput);
+        translationEl.find('.translate-output').text(translationOutput);
+
+        return translationEl;
+    }
+
+    $(document).on('click', '#translate-btn', function() {
+        var text = $('#translate-input').val();
+        var from = $('#start-lang option:selected').data('code');
+        var to = $('#end-lang option:selected').data('code');
+        console.log(text, from, to);
+
+        $.post('/translation', {
+            info: {
+                text: text,
+                from: from,
+                to: to
+            }
+        }, function(data) {
+
+            if (data.toLowerCase() === text.toLowerCase()) {
+                console.log('same foo!')
+            }
+
+            console.log(data);
+
+            $('#translation-container').append(createTranslateEl(from, to, text, data))
 
 
+        })
 
-
-
-
-
-
-
-
-
-
-
+        return false;
+    })
 
 
 
